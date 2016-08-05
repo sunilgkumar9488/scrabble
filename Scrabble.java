a02c603705ac6985faad801600510a4b984170b2
elaiyabharathi <elaiyabharathi@hotmail.com>
2016-08-05 13:36:00 +0530

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -35,15 +35,17 @@ public class Scrabble {
          return perms;
      }
 
-     public static HashMap<String,Integer> scoreEvaluation(String str)
+     public static String scoreEvaluation(String str)
      {
              int a[]=new int[27];
+             int max=0;
+             String s="";
              for(int i=0;i<str.length();i++)
                      a[(str.charAt(i)-'a')]++;
              int b[]=new int[27];
-             HashMap<String,Integer> hm=new HashMap();
+             //HashMap<String,Integer> hm=new HashMap();
              BufferedReader bReader = null;
-             String line = "",s;
+             String line = "";
              try {
                      bReader = new BufferedReader(new FileReader("C:\\Users\\arprasanna\\Desktop\\sowpods.txt"));
                      while((line = bReader.readLine())!=null)
@@ -58,14 +60,18 @@ public class Scrabble {
                              }
                              if(i==str.length())
                              {
-                                     hm.put(line, getScore(line));
+                            	 if(getScore(line)>max)
+                            	 {
+                                     max= getScore(line);
+                                     s=line;	
+                            	 }
                              }
                      }
-             }
+             	}
              catch(Exception e){
             	 
              }
-             return hm;
+             return s;
      }
 
 	
@@ -73,18 +79,18 @@ public class Scrabble {
 
 			//Receiving all possible permutations of tiles
 			
-			HashMap<String,Integer> map = scoreEvaluation(word);
-			ArrayList<String> perms = permutations(word);
-			int max = 0;
-			String maxScoreString = "";
-			for(int i=0;i<perms.size();i++){
-				int score = map.get(word);
-				if(score>max){
-					score = max;
-					maxScoreString = perms.get(i);
-				}
-			}
-            return maxScoreString;
+//			HashMap<String,Integer> map = scoreEvaluation(word);
+//			ArrayList<String> perms = permutations(word);
+//			int max = 0;
+//			String maxScoreString = "";
+//			for(int i=0;i<perms.size();i++){
+//				int score = map.get(word);
+//				if(score>max){
+//					score = max;
+//					maxScoreString = perms.get(i);
+//				}
+//			}
+            return scoreEvaluation(word);
             
 
     }
